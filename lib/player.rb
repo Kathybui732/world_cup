44476bb8d0af305b9184b82358b3660ea5e1102d
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-18 21:34:11 -0600

    Create method for position and pass test

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,8 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :position
 
-  def initialize(params = {})
+  def initialize(params = {name: nil, position: nil})
     @name = params[:name]
+    @position = params[:position]
   end
 end
