95ebd2a8a84b2df08354ae8ace8e3acb65f0f254
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-18 22:22:32 -0600

    Create class and test it exists for world cup

---
+++ lib/world_cup.rb
@@ -0,0 +1,6 @@
+class WorldCup
+
+  def initialize(argument, argument2)
+    @argument = argument
+  end
+end
---
+++ test/world_cup_test.rb
@@ -0,0 +1,26 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/world_cup"
+require "./lib/team"
+require "./lib/player"
+
+class WorldCupTest < Minitest::Test
+
+  def setup
+    @france = Team.new("France")
+    @mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    @pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    @france.add_player(@mbappe)
+    @france.add_player(@pogba)
+    @croatia = Team.new("Croatia")
+    @modric = Player.new({name: "Luka Modric", position: "midfielder"})
+    @vida = Player.new({name: "Domagoj Vida", position: "defender"})
+    @croatia.add_player(@modric)
+    @croatia.add_player(@vida)
+    @world_cup = WorldCup.new(2018, [@france, @croatia])
+  end
+
+  def test_it_can_exist
+    assert_instance_of WorldCup, @world_cup
+  end
+end
