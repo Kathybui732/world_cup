8e1ac50030d030dbcfd394e016aa63231fbda3d1
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-20 20:11:39 -0600

    Create method for active players by position

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -5,4 +5,15 @@ class WorldCup
     @year = year
     @teams = teams
   end
+
+  def active_players_by_position(position)
+    active_players = []
+    @teams.each do |team|
+      active_players << team.players if !team.eliminated?
+    end
+
+    active_players.flatten.select do |player|
+      player.position == position
+    end
+  end
 end
